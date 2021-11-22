3db1eaa4a1caceb38620ee115545e95bdf31f20d
don Pablo <don@obeyi.com>
2021-11-22 07:21:11 +0200

    Init

--- .utils/start-proxy.sh
+++ .utils/start-proxy.sh
@@ -4,7 +4,7 @@ docker rm bedrock-docker-http-proxy
 docker run -d --restart=always \
   -v /var/run/docker.sock:/tmp/docker.sock:ro \
   -v ~/.dinghy/certs:/etc/nginx/certs \
-  -p 80:80 -p 443:443 \
+  -p 8000:80 -p 4430:443 \
   -p 19322:19322/udp \
   -e DNS_IP=127.0.0.1 \
   -e CONTAINER_NAME=bedrock-docker-http-proxy \
